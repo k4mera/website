e3d59663d7ba6ca8c79eeaecf340c68162ed4556
jbmichel14 <jbmichel66@gmail.com>
2025-12-16 00:43:33 +0100

    migrate to astro 1

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        'aileron': ['Aileron', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif']
+      },
+      letterSpacing: {
+        'custom': '2px'
+      }
+    }
+  }
+}
