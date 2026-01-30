2d714304f929fbb9d88114ea0c035ff8c781760c
jbmichel14 <jbmichel66@gmail.com>
2026-01-30 19:03:47 +0100

    SEO and 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   site: 'https://k4mera.world',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('m4gic'),
+    }),
+  ],
 });
